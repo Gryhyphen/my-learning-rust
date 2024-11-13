4bdd51d849da99bf3a8a2ac15968a44c26838909
Gryhyphen <gryhyphen@local>
2024-11-13 07:27:56 +0000

    Somehow avoid having to have an output with a static lifetime

--- MyFirstRustBlinky/embassy_blinky_real/src/main.rs
+++ MyFirstRustBlinky/embassy_blinky_real/src/main.rs
@@ -3,26 +3,23 @@
 use panic_halt as _;
 
 use embassy_executor::{Spawner, Executor};
-use embassy_rp::gpio::{AnyPin, Level, Output};
+use embassy_rp::gpio::{AnyPin, Level, Output, Pin};
 use embassy_rp::init;
 use embassy_time::Timer;
-
-
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let peripherals = init(Default::default());
-
-    let mut onboard_led = Output::new(peripherals.PIN_25, Level::Low);
-    onboard_led.set_high();
     
     spawner
-        .spawn(blink_task(onboard_led))
+        .spawn(blink_task(peripherals.PIN_15.degrade()))
         .unwrap();
 }
 
 #[embassy_executor::task]
-async fn blink_task(mut led: Output<'static>) {
+async fn blink_task(mut led: AnyPin) {
+    let mut led = Output::new(led, Level::Low);
+
     loop {
         led.set_high();
         Timer::after_secs(1).await;
