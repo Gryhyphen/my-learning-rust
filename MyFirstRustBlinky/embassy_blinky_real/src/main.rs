cf6fd6ef7e458ba8fb4f40d6e879fc5f62c420b3
Gryhyphen <gryhyphen@local>
2024-11-13 07:45:54 +0000

    Cleanup code
    Make code as clean as possible

--- MyFirstRustBlinky/embassy_blinky_real/src/main.rs
+++ MyFirstRustBlinky/embassy_blinky_real/src/main.rs
@@ -2,10 +2,14 @@
 #![no_main]
 use panic_halt as _;
 
-use embassy_executor::{Spawner, Executor};
-use embassy_rp::gpio::{AnyPin, Level, Output, Pin};
+use embassy_executor::Spawner;
+use embassy_rp::gpio::Pin;
 use embassy_rp::init;
-use embassy_time::Timer;
+
+
+mod blink_task;
+use blink_task::blink_task;
+
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -14,16 +18,4 @@ async fn main(spawner: Spawner) {
     spawner
         .spawn(blink_task(peripherals.PIN_15.degrade()))
         .unwrap();
-}
-
-#[embassy_executor::task]
-async fn blink_task(mut led: AnyPin) {
-    let mut led = Output::new(led, Level::Low);
-
-    loop {
-        led.set_high();
-        Timer::after_secs(1).await;
-        led.set_low();
-        Timer::after_secs(1).await;
-    }
 }
